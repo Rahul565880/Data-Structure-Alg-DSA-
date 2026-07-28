514efac390f79745aef8205bfb5889422da2364c
Rahul565880 <rp7772633@gmail.com>
2026-07-28 17:41:55 +0530

    Collections  in java

---
+++ src/ArrayList1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        System.out.println("Enter the size of array list");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> al = new ArrayList<>();
+        System.out.println("Enter the elements in array list");
+
+        for(int i=0;i<n;i++) {
+            al.add(sc.nextInt());
+        }
+        System.out.println("Size of " + al.size());
+
+        System.out.print("Elements : " );
+        for(Object x : al) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
